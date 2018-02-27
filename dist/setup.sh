2a8249f807621f57766b96eb150bfd1137fe656a
deesus <d.reddy@yandex.com>
2018-02-27 12:35:53 -0500

    Refactor: remove Gulpfile.

--- dist/setup.sh
+++ dist/setup.sh
@@ -100,147 +100,17 @@ cat<<EOF > package.json
   "main": "index.js",
   "dependencies": {},
   "devDependencies": {
+    "babel-core": "^6.26.0",
+    "babel-loader": "^7.1.3",
+    "webpack": "^4.0.0",
   },
   "scripts": {
+    "build": "webpack"
     "test": "echo \"Error: no test specified\" && exit 1"
   },
   "author": "Dee Reddy",
   "license": "BSD-2-Clause"
 }
-
-EOF
-
-cat <<EOF > Gulpfile.js
-'use strict';
-///////////////////////////////////////////////////
-///
-/// Author: Dee Reddy
-/// Created: 02-16-2017
-/// Last updated: 06-06-2017
-///
-///////////////////////////////////////////////////
-
-/// TODO: gulp sass doesn't pick up changes unless watcher is running
-
-///------------------------------------------------
-/// Import modules:
-///------------------------------------------------
-
-const gulp              = require('gulp');
-const gulpImageResize   = require('gulp-image-resize');	    // See <https://www.npmjs.com/package/gulp-image-resize>
-const gulpRename        = require('gulp-rename');
-const gulpChanged       = require('gulp-changed');
-const concurrent        = require('concurrent-transform');  // will only process files that have changed since last time
-const gulpSass          = require('gulp-sass');
-const autoPrefixer      = require('gulp-autoprefixer');
-const gulpPlumber       = require('gulp-plumber');
-
-///------------------------------------------------
-/// Configuration:
-///------------------------------------------------
-
-/**
- * Config for all Gulp tasks:
- */
-
-const IMG_FILE_TYPES = '*.{jpg,png}';
-const CONFIG = {
-    /// TODO: perhaps it would be more apt to place some of these in a 'RESPONSIVE_CONFIG' objects
-    /// file paths of folders:
-
-    /// To compile .css files files into the same location as .scss files,
-    /// set dest path to same parent folder as src folder (i.e. everything before '**/*.scss').
-    PATHS: {
-	/// TODO: set paths to your project folders:
-        CSS_SRC_PATH:   './local/stylesheets/**/*.css',
-        CSS_DEST_PATH:  './local/stylesheets/',
-        HTML_SRC_PATH:  '',
-        HTML_DEST_PATH: '',
-        IMG_SRC_PATH:   './local/images/images_src/**/' + IMG_FILE_TYPES,
-        IMG_DEST_PATH:  './local/images/images_target',
-        JS_SRC_PATH:    '',
-        JS_DEST_PATH:   '',
-        SASS_SRC_PATH:  './local/stylesheets/**/*.scss',
-        SASS_DEST_PATH: './local/stylesheets/'
-    }
-};
-
-///------------------------------------------------
-/// Tasks:
-///------------------------------------------------
-
-/**
- * Responsive Images
- */
-gulp.task('responsive-images', function () {
-    gulp.src(CONFIG.PATHS.IMG_SRC_PATH)
-        .pipe(gulpPlumber())
-
-        // only process changed files:
-        .pipe(gulpChanged(CONFIG.PATHS.IMG_DEST_PATH))
-
-        // resize images:
-        .pipe(gulpImageResize({
-            width:          1600,   // if we only specify one dimension, output won't exceed this value
-            quality:        0.85,   // jpg quality -- ranges from 0-1
-            crop:           false,
-            upscale:        false,
-            imageMagick:    false   // specifies to use GraphicsMagick instead of ImageMagick
-        }))
-
-        // rename files:
-        .pipe(gulpRename({
-            suffix: '_large_2x'
-        }))
-
-        // destination output:
-        .pipe(gulp.dest(CONFIG.PATHS.IMG_DEST_PATH));
-});
-
-
-/**
- * Compile/process Sass
- */
-gulp.task('sass', function () {
-    gulp.src(CONFIG.PATHS.SASS_SRC_PATH)
-        .pipe(gulpPlumber())
-
-        // only process changed files:
-        .pipe(gulpChanged(CONFIG.PATHS.SASS_SRC_PATH))
-
-        // compile SASS:
-        .pipe(gulpSass({
-                outputStyle: 'expanded'             // specifies formatting of CSS output (see github.com/sass/node-sass#outputstyle)
-            }).on('error', gulpSass.logError)
-        )
-
-        // auto-prefixer:
-        .pipe(autoPrefixer({
-                browsers:   ['last 2 versions'],
-                cascade:    true,                   // visually cascade prefixes
-                flexbox:    'no-2009',              // adds flexbox prefixes ONLY for final and IE versions
-                grid:       true                    // adds IE prefixes for Grid Layouts
-            }).on('error', function (error) {
-                console.log(error.message);
-        }))
-
-        // destination output:
-        .pipe(gulp.dest(CONFIG.PATHS.SASS_DEST_PATH));
-});
-
-
-/**
- * Watcher
- *
- * TODO: add your tasks to here
- */
-gulp.task('watch', function() {
-    gulp.watch(CONFIG.PATHS.SASS_SRC_PATH, ['sass']);
-});
-
-
-/// default task:
-gulp.task('default', ['sass', 'watch']);
 
 EOF
 
